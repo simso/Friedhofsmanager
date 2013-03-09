e03aeee393aed643d5058a39160a63c23802c5e2
dirk <jai.white@gmx.de>
2013-03-09 21:07:30 +0100

    Titel vom Fenster geändert

--- src/de/fhflensburg/graveyardmanager/core/GraveyardManagerGame.java
+++ src/de/fhflensburg/graveyardmanager/core/GraveyardManagerGame.java
@@ -26,7 +26,7 @@ import java.util.List;
 public class GraveyardManagerGame extends NiftyStateBasedGame
 {
 	/** The name of the game */
-	public static final String GAMETITLE = "Hodie mihi, Cras tibi - Der Friedhofsmanager";
+	public static final String GAMETITLE = "Das Spiel mit dem Tod - Der Friedhofsmanager";
 
 	/** The current version of the game */
 	public static final String VERSION = "1.0.0-Alpha";
